30b66d410ed7471b1c8e08bf68039f8d29d8389c
Petra-78 <partos.petra.0305@gmail.com>
2025-07-24 13:09:02 +0200

    add append function

--- LinkedLists.js
+++ LinkedLists.js
@@ -5,6 +5,19 @@ class LinkedList {
     constructor() {
         this.head = null;
     }    
+
+    append(value) {
+        const newNode = new Node(value)
+        if (this.head === null) {
+            this.head = newNode
+            return
+        }
+        let current = this.head
+        while (current.nextNode !== null) {
+            current = current.nextNode
+        }
+        current.nextNode = newNode
+    }
 }
    
 
